2daee88adb9ec0a81b504edfd8187262b1fadbc1
Thomas <thomas.ansart@stilva.com>
2019-02-08 12:06:37 -0500

    including builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
 
 export default {
-  input: 'src/index.js',
+  input: 'src/Autocomplete.js',
   output: [
     {
       file: './dist/autocomplete.js',
