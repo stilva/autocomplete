ea92e5160fb3ac39894b0f23ddab319167837909
Thomas <thomas.ansart@stilva.com>
2019-02-08 11:01:52 -0500

    initial Request implementation

--- src/Request.js
+++ src/Request.js
@@ -1,1 +1,36 @@
-export default class Request {}
+export default class Request {
+  constructor(uri) {
+    const scope = this;
+    const xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function(e) {
+      scope._onComplete.call(scope, this.response);
+    });
+
+    xhr.addEventListener('error', function(e) {
+      try {
+        scope._onError.call(scope, this.response);
+      } catch(e) {}
+    });
+
+    xhr.open('GET', uri);
+
+    xhr.send();
+
+    this._xhr = xhr;
+  }
+
+  abort() {
+    this._xhr.abort();
+  }
+
+  onComplete(callback) {
+    this._onComplete = callback;
+  }
+
+  onError(callback) {
+    this._onError = callback;
+  }
+}
